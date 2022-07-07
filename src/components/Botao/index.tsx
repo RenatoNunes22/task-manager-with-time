d88a5469e0cb2bfdf21c443de61dec288060bc13
Renato Nunes <97243555+RenatoNunes22@users.noreply.github.com>
2022-07-07 18:38:08 -0300

    Initial commit

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Botao.scss";
+class Botao extends React.Component {
+  render() {
+    return <button className="botao">Adicionar</button>;
+  }
+}
+
+export default Botao;
---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Botao from "../Botao";
+import "./Formulario.scss";
+class Formulario extends React.Component {
+  render() {
+    return (
+      <form className="novaTarefa">
+        <div className="inputContainer">
+          <label htmlFor="tarefa">Adicione uma tarefa</label>
+          <input
+            type="text"
+            name="tarefa"
+            id="tarefa"
+            placeholder="O que você quer estudar ?"
+            required
+          />
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="tempo">Tempo</label>
+          <input
+            type="time"
+            step="1"
+            name="tempo"
+            id="tempo"
+            min="00:00:00"
+            max="02:00:00"
+          />
+        </div>
+        <Botao />
+      </form>
+    );
+  }
+}
+export default Formulario;
